d49cc37737a0e3dadef65074e62b278660876e59
Aily Fernandez <ailyfdez89@gmail.com>
2021-09-06 14:03:28 -0300

    hidde location

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<events-list></events-list>`
+  template: `
+  <navbar></navbar>
+  <events-list></events-list>`
 })
 export class AppComponent {
   title = 'ng-fundamentals';
